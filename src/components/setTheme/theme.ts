6496ce46e97478a9d8b32d3c4ad1d87fa7fd72a7
hejueting <279975246@qq.com>
2021-02-24 23:53:19 +0800

    回到顶部按钮组件的主题色配置

--- src/components/setTheme/theme.ts
+++ src/components/setTheme/theme.ts
@@ -3,4 +3,8 @@ export default {
 	footer_bg_color: "#24292e",
 	footer_font_color: "#fff",
 	footer_font_hover_color: "rgba(255,255,255,0.7)",
+	// backTop
+	backTop_color: "#fff",
+	backTop_bg_color: "rgba(0,0,0,0.65)",
+	backTop_bg_hover_color: "rgba(0,0,0,0.55)",
 };
