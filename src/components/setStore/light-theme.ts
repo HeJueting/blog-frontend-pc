9363fc0a0b094756078cb50fe9d6d9a2838d81ee
hejueting <279975246@qq.com>
2021-02-26 09:26:00 +0800

    message、modal组件的主题配置

--- src/components/setStore/light-theme.ts
+++ src/components/setStore/light-theme.ts
@@ -1,42 +1,48 @@
 // 白天主题
 export default {
-	// 全局
-	font_color: "#586069",
-	font_blue_color: "#0366d6",
-	border_color: "#e1e4e8",
-	bg_color: "#fff",
-	bg_hover_color: "#f6f8fa",
-	placeholder_color: "rgba(0, 0, 0, 0.3)",
+    // 全局
+    font_color: "#586069",
+    font_blue_color: "#0366d6",
+    border_color: "#e1e4e8",
+    bg_color: "#fff",
+    bg_hover_color: "#f6f8fa",
+    placeholder_color: "rgba(0, 0, 0, 0.3)",
 
-	// 滚动条
-	scrollbar_bg_color: "rgb(241,241,241)",
-	scrollbar_thumb_color: "rgb(183,183,183)",
+    // 滚动条
+    scrollbar_bg_color: "rgb(241,241,241)",
+    scrollbar_thumb_color: "rgb(183,183,183)",
 
-	// 菜单
-	menu_bg_color: "#101315",
-	menu_bg_hover_color: "#791a1f",
-	menu_font_color: "#aaa",
-	menu_font_hover_color: "#fff",
-	menu_mask_bg_color: "rgba(0, 0, 0, 0.3)",
-	menu_icon_bg_color: "rgba(0,0,0,0.8)",
+    // 菜单
+    menu_bg_color: "#101315",
+    menu_bg_hover_color: "#791a1f",
+    menu_font_color: "#aaa",
+    menu_font_hover_color: "#fff",
+    menu_mask_bg_color: "rgba(0, 0, 0, 0.3)",
+    menu_icon_bg_color: "rgba(0,0,0,0.8)",
 
-	// footer
-	footer_bg_color: "#24292e",
-	footer_font_color: "#fff",
-	footer_font_hover_color: "rgba(255,255,255,0.7)",
+    // footer
+    footer_bg_color: "#24292e",
+    footer_font_color: "#fff",
+    footer_font_hover_color: "rgba(255,255,255,0.7)",
 
-	// backTop
-	backTop_color: "#fff",
-	backTop_bg_color: "rgba(0,0,0,0.65)",
-	backTop_bg_hover_color: "rgba(0,0,0,0.55)",
+    // backTop
+    backTop_color: "#fff",
+    backTop_bg_color: "rgba(0,0,0,0.65)",
+    backTop_bg_hover_color: "rgba(0,0,0,0.55)",
 
-	// loading
-	loading_color: "rgba(0,0,0,0.85)",
-	loading_bg_color: "rgba(255, 255, 255, 0.85)",
+    // loading
+    loading_color: "rgba(0,0,0,0.85)",
+    loading_bg_color: "rgba(255, 255, 255, 0.85)",
 
-	// themeSwitcher
-	themeSwitcher_bg_color: "#fff",
-	themeSwitcher_border_color: "#d1d5da",
-	themeSwitcher_thumb_bg_color: "#2f363d",
-	themeSwitcher_thumb_icon_color: "#ffdf5d",
+    // themeSwitcher
+    themeSwitcher_bg_color: "#fff",
+    themeSwitcher_border_color: "#d1d5da",
+    themeSwitcher_thumb_bg_color: "#2f363d",
+    themeSwitcher_thumb_icon_color: "#ffdf5d",
+
+    // message
+    message_shadow_color: "rgba(255,255,255,0.3)",
+
+    // modal
+    modal_mask_color: "rgba(255,255,255,0.6)",
 };
