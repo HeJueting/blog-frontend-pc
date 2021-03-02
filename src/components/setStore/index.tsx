578f58781911f93972b402bb53037ebd6be30a0a
hejueting <279975246@qq.com>
2021-03-02 10:20:35 +0800

    博客主题从服务端配置,优化无数据时的展示

--- src/components/setStore/index.tsx
+++ src/components/setStore/index.tsx
@@ -4,51 +4,42 @@ import settingAxios from "../../../src/api/setting";
 import userAxios from "../../../src/api/user";
 import { updateTheme } from "../../utils/help";
 
-import darkTheme from "./dark-theme";
-import lightTheme from "./light-theme";
-
-const themeConfig = {
-	light: lightTheme,
-	dark: darkTheme,
-};
-
 const SetStore: () => null = () => {
-	const { dispatch } = useContext(context);
+    const { dispatch } = useContext(context);
 
-	// 初始化store
-	const initStore = async () => {
-		// 博客配置项
-		const settingRes = await settingAxios.search();
-		if (settingRes.code === 0) {
-			settingRes.data.themeConfig = themeConfig;
-			// 初始化博客配置
-			dispatch({
-				type: "SET_SETTINGINFO",
-				params: {
-					settingInfo: settingRes.data,
-				},
-			});
-			// 初始化主题配置（默认为白天主题）
-			updateTheme("light", dispatch);
-		}
-		// 用户基本信息
-		const userRes = await userAxios.info();
-		if (userRes.code === 0) {
-			dispatch({
-				type: "SET_USERINFO",
-				params: {
-					userInfo: userRes.data,
-				},
-			});
-		}
-	};
+    // 初始化store
+    const initStore = async () => {
+        // 博客配置项
+        const settingRes = await settingAxios.search();
+        if (settingRes.code === 0) {
+            // 初始化博客配置
+            dispatch({
+                type: "SET_SETTINGINFO",
+                params: {
+                    settingInfo: settingRes.data,
+                },
+            });
+            // 初始化主题配置（默认为白天主题）
+            updateTheme("light", dispatch);
+        }
+        // 用户基本信息
+        const userRes = await userAxios.info();
+        if (userRes.code === 0) {
+            dispatch({
+                type: "SET_USERINFO",
+                params: {
+                    userInfo: userRes.data,
+                },
+            });
+        }
+    };
 
-	// 初始化博客配置信息
-	useEffect(() => {
-		initStore();
-	}, []);
+    // 初始化博客配置信息
+    useEffect(() => {
+        initStore();
+    }, []);
 
-	return null;
+    return null;
 };
 
 export default SetStore;
