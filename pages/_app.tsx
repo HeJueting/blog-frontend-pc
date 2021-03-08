5d7a30bc5cea3de3f3f44af26d4f2cd744b4d6b6
hejueting <279975246@qq.com>
2021-03-08 22:09:08 +0800

    搭配nginx，优化本地运行配置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,78 +20,78 @@ import ErrorCapture from "../src/components/errorCapture";
 
 // 接口：props
 interface IAppProps {
-    Component: any;
-    pageProps: any;
+	Component: any;
+	pageProps: any;
 }
 
 const App: React.FC<IAppProps> = ({ Component, pageProps }) => {
-    // 中间内容区域
-    const mainWrapRef = useRef(null);
-    // router
-    const router = useRouter();
-    // 菜单切换
-    const [visible, setVisible] = useState(false);
-    // 是否展示footer
-    const noFooterRouters = ["/", "/resume", "/login"];
+	// 中间内容区域
+	const mainWrapRef = useRef(null);
+	// router
+	const router = useRouter();
+	// 菜单切换
+	const [visible, setVisible] = useState(false);
+	// 是否展示footer
+	const noFooterRouters = ["/", "/resume", "/login"];
 
-    useEffect(() => {
-        // 路由监听
-        router.events.on("routeChangeStart", () => {
-            // 展示loading
-            window.$setLoading(true);
-        });
-        router.events.on("routeChangeComplete", () => {
-            // loadin消失
-            window.$setLoading(false);
-        });
-    }, []);
+	useEffect(() => {
+		// 路由监听
+		router.events.on("routeChangeStart", () => {
+			// 展示loading
+			window.$setLoading(true);
+		});
+		router.events.on("routeChangeComplete", () => {
+			// loadin消失
+			window.$setLoading(false);
+		});
+	}, []);
 
-    return (
-        <Provider>
-            <div className={style["wrap"]}>
-                <Head>
-                    <script src="/js/bubbly-bg.js"></script>
-                </Head>
+	return (
+		<Provider>
+			<div className={style["wrap"]}>
+				<Head>
+					<script src="/blog/js/bubbly-bg.js"></script>
+				</Head>
 
-                {/* 导航栏 */}
-                <Menu visible={visible} setVisible={setVisible} />
+				{/* 导航栏 */}
+				<Menu visible={visible} setVisible={setVisible} />
 
-                {/* 设置主题 */}
-                <SetTheme />
+				{/* 设置主题 */}
+				<SetTheme />
 
-                {/* 设置全局store */}
-                <SetStore />
+				{/* 设置全局store */}
+				<SetStore />
 
-                {/* 设置页面font-size */}
-                <SetRem />
+				{/* 设置页面font-size */}
+				<SetRem />
 
-                {/* 页面错误信息监听 */}
-                <ErrorCapture />
+				{/* 页面错误信息监听 */}
+				<ErrorCapture />
 
-                {/* Loading */}
-                <Loading />
+				{/* Loading */}
+				<Loading />
 
-                {/* 页面正文内容 */}
-                <main
-                    id="scroll-content"
-                    ref={mainWrapRef}
-                    className={`${style["wrap-main"]} ` + (visible ? style["wrap-main-out"] : style["wrap-main-in"])}
-                >
-                    {/* 主题模式切换 */}
-                    <ThemeSwitcher />
+				{/* 页面正文内容 */}
+				<main
+					id="scroll-content"
+					ref={mainWrapRef}
+					className={`${style["wrap-main"]} ` + (visible ? style["wrap-main-out"] : style["wrap-main-in"])}
+				>
+					{/* 主题模式切换 */}
+					<ThemeSwitcher />
 
-                    {/* 页面组件 */}
-                    <Component {...pageProps} />
+					{/* 页面组件 */}
+					<Component {...pageProps} />
 
-                    {/* footer */}
-                    {noFooterRouters.indexOf(router.pathname) === -1 && <Footer />}
-                </main>
+					{/* footer */}
+					{noFooterRouters.indexOf(router.pathname) === -1 && <Footer />}
+				</main>
 
-                {/* 回到顶部 */}
-                <BackTop />
-            </div>
-        </Provider>
-    );
+				{/* 回到顶部 */}
+				<BackTop />
+			</div>
+		</Provider>
+	);
 };
 
 export default App;
