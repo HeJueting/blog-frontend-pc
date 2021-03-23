6857cc3b2ec054363d137941a711c7617525c8e3
hejueting <279975246@qq.com>
2021-03-23 10:23:55 +0800

    日志目录调整

--- src/api/error.ts
+++ src/api/error.ts
@@ -1,15 +1,15 @@
-import Axios from "../utils/axios";
-import { IErrorCollect } from "../typing/api/error";
+import Axios from '../utils/axios';
+import { IErrorCollect } from '../typing/api/error';
 
 const errorAxios = {
     // 收集错误日志
     collect: (data: IErrorCollect) =>
         Axios({
-            method: "post",
-            url: "/error/frontend/collect",
+            method: 'post',
+            url: '/error/frontend/collect',
             data: {
                 msg: data.msg,
-                type: "web-frontend-log",
+                type: 'web-frontend',
             },
         }),
 };
